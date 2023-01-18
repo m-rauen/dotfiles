04819a3796c9a8699f3f1e08f00b669ea06219f0
Matheus Rauen C. L <matheus.rauen@grad.ufsc.br>
2023-01-18 16:16:03 -0300

    Added pip script to upgd all

---
+++ pip-upgd-all.py
@@ -0,0 +1,6 @@
+#https://stackoverflow.com/a/5839291
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+call("pip install --upgrade " + ' '.join(packages), shell=True)
