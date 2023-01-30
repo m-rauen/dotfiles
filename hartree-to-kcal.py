b286a70e8b379faefbcd279156ba1f021ecd7fd9
Matheus Rauen C. L <matheus.rauen@grad.ufsc.br>
2023-01-30 17:35:17 -0300

    Updated hartree-to-kcal script

--- hartree-to-kcal.py
+++ hartree-to-kcal.py
@@ -1,22 +1,51 @@
+import os
+
 fname = input('File name: ')
 
-energy_data = {}
-count = 1
+energy_data = []
+
+while True:
+    try:
+        if (os.stat(fname).st_size == 0):
+            raise IOError('File empty error!')
+        else:
+            break
+    except IOError:
+        print("""
+              ***************************
+              ERROR: Empty file selected!
+              ***************************
+              """)
+        quit()
 
 for lines in open(fname):
     if lines is None:
-        quit()
+        try:
+            raise ValueError('Empty line error!')
+        except ValueError:
+            continue
     else:
-        energy = float(lines)
-        new_energ = round(energy * 627.50, 3)
-        energy_data.update({count:new_energ})
-        count += 1
-
-fhandler = open('conv-results.dat', 'w')        
-for k,v in energy_data.items():
-    fhandler.write(str(k) + '   ' + str(v))
+        new_lines = lines.rstrip()
+        words = new_lines.split()
+        for w in words:
+            if (w.isalpha() == True):
+                try:
+                    raise ValueError('Words error!')
+                except ValueError:
+                    continue
+            else:
+                if (w.isalnum() == True): 
+                    energy = float(w)
+                    new_energ = round(energy * 627.50, 3)
+                    energy_data.append(new_energ)
+                else:
+                    energy_data.append(w)
+                
+fhandler = open('converted_results.dat', 'w')        
+for e in energy_data:
+    fhandler.write(str(e))
     fhandler.write('\n')
     
-print('Everything done!\n'
+print('All done!\n'
       'Enjoy your data :)')
 
