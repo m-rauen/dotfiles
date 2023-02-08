5b43b55bc10c49aa8b320746d9bb96b3e657a6c5
Matheus Rauen C. L <matheus.rauen@grad.ufsc.br>
2023-02-08 15:49:58 -0300

    Updated configs

---
+++ custom.aliases.bash
@@ -0,0 +1,33 @@
+# apt aliases 
+alias aptclean='sudo apt clean'
+alias aptautorm='sudo apt autoremove'
+alias pkgf='dpkg --listfiles'
+alias pkgl='dpkg --list'
+alias pkglist='dpkg --list'
+
+# tmux aliases 
+alias tmx='tmux'
+alias tlist='tmux ls'
+alias tnew='tmux new-session -s'
+alias tattach='tmux a -t'
+alias tkill='tmux kill-session -t'
+alias trename='tmux rename-session -t'
+
+# vim aliases 
+alias vver='vim --version'
+
+#git aliases 
+alias gi='git init -b master'
+alias ginit='git init -b master'
+alias gimain='git init -b main'
+
+alias gadd='git add .'
+
+alias gc='git commit -v -m'
+
+alias gclone='git clone'
+
+alias gmerge='git merge'
+
+alias gpll='git pull'
+alias gpull='git pull'
