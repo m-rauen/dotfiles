b0e0f18fe6749071eaecfb5e9b8dad8f35bf71d5
Matheus Rauen <matheus.rauen@grad.ufsc.br>
2023-03-06 12:33:30 -0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -18,18 +18,18 @@ install(){
         command_already_exists $package
     fi
 }
-
+eval "cp .bash_aliases ~/" 
+eval "cp .bash_profile ~/"
 install "vim" "sudo apt install vim"
-install "git" "sudo apt install git"
 install "curl" "sudo apt install curl"
 install "okular" "sudo apt install okular"
 install "gnome-tweaks" "sudo add-apt-repository universe && sudo apt install gnome-tweaks"
 install "gnome-extensions" "sudo apt install gnome-shell-extensions"
 install "bash-it" "git clone --depth=1 https://github.com/Bash-it/bash-it.git
-~/.bash_it && mv custom.aliases.bash ~/.bash_it/aliases/ && cd ~/.bash_it/install.sh &&"
-install "starship" "curl -sS https://starship.rs/install.sh | sh"
+~/.bash_it && cp custom.aliases.bash ~/.bash_it/aliases/ && cd ~/.bash_it/install.sh &&"
+install "starship" "curl -sS https://starship.rs/install.sh | sh && cp .config/starship.toml ~/.config/"
 install "gogh" "git clone https://github.com/Gogh-Co/Gogh.git ~/.gogh"
-install "tmux" "sudo apt install tmux"
+install "tmux" "sudo apt install tmux && mv .tmux.conf ~/"
 install "ctags" "sudo apt install universal-ctags"
 install "pip" "sudo apt install python3-pip"
 install "latex" "sudo apt install texlive"
