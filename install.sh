7479b061dc82479ca2c21f07dff6dfb3f648e475
Matheus Rauen C. L <matheus.rauen@grad.ufsc.br>
2023-03-14 10:14:28 -0300

    Making install.sh consider my backup first

--- install.sh
+++ install.sh
@@ -21,8 +21,11 @@ install(){
 
 eval "cp .bash_aliases ~/" 
 eval "cp .bash_profile ~/"
-eval "rm ~/.config/Code/User/settings.json && cp .config/settings.json ~/.config/Code/User/"
-install "firacode" "sudo apt install fonts-firacode"
+eval "cp pip-upgd-all.py ~/"
+eval "cp hartree-to-kcal.py ~/"
+install "nerd-fonts" "git clone --depth 1
+https://github.com/ryanoasis/nerd-fonts.git ~/.nerd-fonts"
+eval "cd ~/.nerd-fonts"
 install "vim" "sudo apt install vim"
 install "curl" "sudo apt install curl"
 install "okular" "sudo apt install okular"
@@ -31,15 +34,18 @@ install "gnome-tweaks" "sudo apt install gnome-tweaks"
 install "gnome-extensions" "sudo apt install gnome-shell-extensions"
 install "bash-it" "git clone --depth=1 https://github.com/Bash-it/bash-it.git
 ~/.bash_it" 
-eval "cp custom.aliases.bash ~/.bash_it/aliases/" 
+
 install "starship" "curl -sS https://starship.rs/install.sh | sh" 
-eval "cp .config/starship.toml ~/.config/"
+
 install "gogh" "git clone https://github.com/Gogh-Co/Gogh.git ~/.gogh"
 install "tmux" "sudo apt install tmux" 
-eval "cp .tmux.conf ~/"
 install "ctags" "sudo apt install universal-ctags"
 install "pip" "sudo apt install python3-pip"
 install "latex" "sudo apt install texlive"
 install "latex_pt-br" "sudo apt install texlive-lang-portuguese"
+
+eval "cd ~/.nerd-fonts && ./install.sh"
+eval "cp .config/starship.toml ~/.config/"
+eval "cp .tmux.conf ~/"
 
 
